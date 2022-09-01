13988bbd857edee01889ffe02824a53761cae33b
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-09-01 15:59:52 -0700

    working on authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  resources :users
   namespace :api do
     namespace :v1 do
       resources :products
     end
   end
   
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_26_182355) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_01_214219) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_26_182355) do
     t.string "tipo"
     t.text "description"
     t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
